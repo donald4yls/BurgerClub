f995a3471d5c74390c92609a2612b5c49716501f
donaldyls <donaldyls@sina.com>
2023-03-19 21:58:18 +0800

    [20230319]Add BurgerClub Front-end UI.

---
+++ BurgerClubFE/src/Mock/site.js
@@ -0,0 +1,61 @@
+const Mock = require('mockjs');
+export default [
+    // 站点信息
+    {
+        url: '/site',
+        type: 'get',
+        response: () => {
+            return {
+                code: 20000,
+                data: {
+                    avatar: 'https://s2.ax1x.com/2020/01/17/1SCadg.png',
+                    slogan: 'Enjoy your burgers.',
+                    name: 'My Burger Club',
+                    domain: '',
+                    notice: 'Enjoy your burgers.',
+                    desc: 'Enjoy your burgers.'
+                }
+            }
+        }
+    },
+    // 站点社交信息
+    {
+        url: '/social',
+        type: 'get',
+        response: () => {
+            return {
+                code: 20000,
+                data: [
+                    {
+                        id: 1,
+                        title: 'QQ',
+                        icon: 'iconqq',
+                        color: '#1AB6FF ',
+                        href: 'http://wpa.qq.com/msgrd?v=3&uin=1224971566&site=qq&menu=yes'
+                    },
+                    {
+                        id: 2,
+                        title: 'Gitee',
+                        icon: 'icongitee',
+                        color: '#d81e06',
+                        href: 'https://gitee.com/fengziy'
+                    },
+                    {
+                        id: 3,
+                        title: 'GitHub',
+                        icon: 'icongithub',
+                        color: '',
+                        href: 'https://github.com/fengziye'
+                    },
+                    {
+                        id: 4,
+                        title: 'CSDN',
+                        icon: 'iconcsdn',
+                        color: 'red',
+                        href: 'https://blog.csdn.net/feng_zi_ye'
+                    }
+                ]
+            }
+        }
+    }
+]
